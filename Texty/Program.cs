0a299d5f294ac10befd619483763691c394c5584
Sylo369 <mitkokuzmanov@gmail.com>
2024-02-01 02:09:11 +0200

    first commit
    to be edited

---
+++ Texty/Program.cs
@@ -0,0 +1,8 @@
+﻿StreamWriter writer = new StreamWriter(@"../../../test.txt");
+StreamReader reader = new StreamReader(@"../../../read.txt");
+writer.Write("If you are reading this message from the text file, this means you have written it through the console succesfully.");
+Console.WriteLine(reader.ReadLine()); 
+
+
+
+writer.Close();
